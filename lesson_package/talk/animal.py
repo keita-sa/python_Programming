dd9387ffe7c7dafe61a2fee16581660bffc0c5cb
桜井啓太 <keitasakuraims@gmail.com>
2021-08-04 15:58:25 +0900

    Initial commit

---
+++ lesson_package/talk/animal.py
@@ -0,0 +1,12 @@
+from lesson_package.tools import utils
+
+def sing():
+    return '##hsuhdsfghassing'
+
+def cry():
+    return utils.say_twice('hahgskfjsliejfs')
+
+
+if __name__ == '__main__':
+    print(sing())
+    print('animal:', __name__)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python_Programming',
+    version='1.0.0',
+    packages=['lesson_package', 'lesson_package.talk', 'lesson_package.tools'],
+    url='',
+    license='Free',
+    author='keitasakurai',
+    author_email='',
+    description='Sample package'
+)
